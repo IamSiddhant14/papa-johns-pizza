c643162261bf8849965d8de04fedaef8abead106
Siddhant <siddhantsharma669@gmail.com>
2022-09-23 14:52:51 +0530

    Created Navbar and added Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import './App.css';
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import Navigation from './components/Navigation'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
+const App = () => {
+
   return (
-     <h1>Hi</h1>
-  );
+
+    <Router>
+      <Navigation/>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+      </Routes>
+    </Router>
+
+  )
+
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () =>{
+   return (
+    <h1>Hello from Home component</h1>
+   )
+}
+
+export default Home
