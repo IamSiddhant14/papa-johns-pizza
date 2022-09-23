83452d99ab66dac659cfa4a6b6593f46bbba11cd
Siddhant <siddhantsharma669@gmail.com>
2022-09-24 01:49:30 +0530

    Added product and single product componenet

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navigation from './components/Navigation'
-import Products from './components/Products'
+import ProductsPage from './pages/ProductsPage'
 import Cart from './pages/Cart'
+import SingleProduct from './pages/SingleProduct';
 
 import {
   BrowserRouter as Router,
@@ -20,10 +21,11 @@ const App = () => {
       <Navigation/>
       <Routes>
 
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products/>} />
-        <Route path="/cart" element={<Cart/>} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" exact element={<About />} />
+        <Route path="/products" exact element={<ProductsPage/>} />
+        <Route path="/cart" exact element={<Cart/>} />
+        <Route path="/products/:_id" exact element={<SingleProduct />} />
 
       </Routes>
     </Router>
