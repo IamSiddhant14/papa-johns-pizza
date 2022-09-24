691b5a5da91f8006e360fb43a297e5caf2891451
Siddhant <siddhantsharma669@gmail.com>
2022-09-25 00:13:25 +0530

    Added more routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import { CartContext } from '../CartContext';
+import '../index.css';
 
 const Navigation = () => {
 
@@ -20,19 +21,31 @@ const Navigation = () => {
 <nav className="container mx-auto flex items-center justify-between py-4">
 
                 <Link to="/">
+
                     <img style={{ height: 45 }} src="/images/logo.png" alt="logo" />
+
                 </Link>
 
                 <ul className="flex items-center">
-                    <li><Link to="/">Home</Link></li>
-                    <li className="ml-6"><Link to="/products">Products</Link></li>
+
+                    <li><Link to="/" className="ml-6, box" >Home</Link></li>
+
+                    <li className="ml-6 , box"><Link to="/products">Products</Link></li>
+
                     <li className="ml-6">
+
                         <Link to="/cart">
+
                             <div style={cartStyle}>
+                                
                                 <span>{cart.totalItems ? cart.totalItems : 0 }</span>
+
                                 <img className="ml-2" src="/images/cart.png" alt="cart-icon" />
+
                             </div>
+
                         </Link>
+
                     </li>
                 </ul>
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import Products from '../components/Products';
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
@@ -8,7 +9,7 @@ const Home = () => {
                 <div className="w-1/2">
                     <h6 className="text-lg"><em>Are you hungry?</em></h6>
                     <h1 className="text-3xl md:text-6xl font-bold">Don't wait !</h1>
-                    <button className="px-6 py-2 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600">Order Now</button>
+                    <Link to="/products"><button className="px-6 py-2 rounded-full text-white font-bold mt-4 bg-yellow-500 hover:bg-yellow-600" >Order Now</button></Link>
                 </div>
                 <div className="w-1/2">
                     <img className="w-4/5" src="/images/pizza.png" alt="pizza" />
