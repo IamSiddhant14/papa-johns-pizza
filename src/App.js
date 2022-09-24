f205c498e7f4fccfe136b34377debb321447b4ef
Siddhant <siddhantsharma669@gmail.com>
2022-09-24 22:23:32 +0530

    Initial version prepared for deployemnt with no known bugs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navigation from './components/Navigation'
 import ProductsPage from './pages/ProductsPage'
 import Cart from './pages/Cart'
 import SingleProduct from './pages/SingleProduct';
+import { CartContext } from './CartContext';
+import { useState , useEffect } from 'react';
+import { getCart, storeCart } from './helpers';
 
 import {
   BrowserRouter as Router,
@@ -15,19 +17,37 @@ import {
 
 const App = () => {
 
+  const [ cart , setCart ] = useState({});
+
+  useEffect(() => {
+     getCart().then( cart => {
+      setCart(JSON.parse(cart))
+     });
+     
+  } , [] )
+
+  useEffect(() => {
+    storeCart(JSON.stringify(cart));
+      
+ } , [cart] )
+
   return (
 
     <Router>
-      <Navigation/>
-      <Routes>
+      <CartContext.Provider value = {{cart:cart , setCart}}>
 
-        <Route path="/" exact element={<Home />} />
-        <Route path="/about" exact element={<About />} />
-        <Route path="/products" exact element={<ProductsPage/>} />
-        <Route path="/cart" exact element={<Cart/>} />
-        <Route path="/products/:_id" exact element={<SingleProduct />} />
+        <Navigation />
+        <Routes>
 
-      </Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" exact element={<About />} />
+          <Route path="/products" exact element={<ProductsPage />} />
+          <Route path="/cart" exact element={<Cart />} />
+          <Route path="/products/:_id" exact element={<SingleProduct />} />
+
+        </Routes>
+
+      </CartContext.Provider>
     </Router>
 
   )
--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -14,6 +14,7 @@ const SingleProduct = () => {
             console.log(product)
             setProduct(product);
         })
+         
     }, [params._id]);
 
 
